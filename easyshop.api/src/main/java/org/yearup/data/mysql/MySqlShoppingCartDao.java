2dc2e583f92ff2c688f0f2dbab82a29b3908c631
Shiraaz Ahmed <shiraaza08@gmail.com>
2025-06-25 14:11:54 -0400

    refactor: improve code structure and add authentication checks in ShoppingCartController

---
+++ easyshop.api/src/main/java/org/yearup/data/mysql/MySqlShoppingCartDao.java
@@ -0,0 +1,117 @@
+package org.yearup.data.mysql;
+
+import org.springframework.stereotype.Repository;
+import org.yearup.data.ShoppingCartDao;
+import org.yearup.models.ShoppingCart;
+import org.yearup.models.ShoppingCartItem;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class MySqlShoppingCartDao implements ShoppingCartDao {
+
+    private final DataSource dataSource;
+
+    public MySqlShoppingCartDao(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    public ShoppingCart getByUserId(int userId) {
+        return getCart(userId);
+    }
+
+    @Override
+    public ShoppingCart getCart(int userId) {
+        String sql = "SELECT * FROM shopping_cart WHERE user_id = ?";
+        ShoppingCart cart = new ShoppingCart();;
+        Map<Integer, ShoppingCartItem> items = new HashMap<>();
+
+        try (Connection connection = dataSource.getConnection();
+        PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setInt(1, userId);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                int productId = resultSet.getInt("product_id");
+                int quantity = resultSet.getInt("quantity");
+                ShoppingCartItem item = new ShoppingCartItem();
+                item.setProductId(productId);
+                item.setQuantity(quantity);
+                items.put(productId, item);
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        cart.setItems(items);
+        return cart;
+    }
+
+    @Override
+    public ShoppingCart addProduct(int userId, int productId) {
+        String sql = """
+            INSERT INTO shopping_cart (user_id, product_id, quantity)
+            VALUES (?, ?, 1);
+        """;
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setInt(1, userId);
+            statement.setInt(2, productId);
+            statement.executeUpdate();
+//            ResultSet resultSet = statement.executeQuery();
+//            if (resultSet.next()) {
+//                try (PreparedStatement updateStatement = connection.prepareStatement(sql)) {
+//                    updateStatement.setInt(1, userId);
+//                    updateStatement.setInt(2, productId);
+//                    updateStatement.executeUpdate();
+//                }
+//            }
+//            else {
+//                try (PreparedStatement insertStatement = connection.prepareStatement(sql)) {
+//                    insertStatement.setInt(1, userId);
+//                    insertStatement.setInt(2, productId);
+//                    insertStatement.executeUpdate();
+//                }
+//            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return getCart(userId);
+    }
+
+    @Override
+    public ShoppingCart updateProduct(int userId, int productId, int quantity) {
+        String sql = "UPDATE shopping_cart SET quantity = ? WHERE user_id = ? AND product_id = ?";
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setInt(1, quantity);
+            statement.setInt(2, userId);
+            statement.setInt(3, productId);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return getCart(userId);
+    }
+
+    @Override
+    public ShoppingCart deleteProduct(int userId)  {
+        String sql = "DELETE FROM shopping_cart WHERE user_id = ?";
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setInt(1, userId);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return getCart(userId);
+    }
+}
